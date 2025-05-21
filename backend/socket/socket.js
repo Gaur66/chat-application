9563aa0109fc274c4a550c9ff9265582106e94e2
Gaurav Kumar singh <132016583+Gaur66@users.noreply.github.com>
2025-05-21 19:37:23 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server= http.createServer(app)
 
 const io= new Server(server,{
     cors:{
-  origin:'http://localhost:5173'
+  origin:'https://chit-chat-14z7.onrender.com'
     }
 })
 const userSocketMap={}
